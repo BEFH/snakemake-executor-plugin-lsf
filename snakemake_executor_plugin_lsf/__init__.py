4a1f1c582ca05ba077ce9779ef2ca807c5a4b5d5
David Laehnemann <1379875+dlaehnemann@users.noreply.github.com>
2026-08-21 13:49:57 +0200

    chore: fix linting

--- snakemake_executor_plugin_lsf/__init__.py
+++ snakemake_executor_plugin_lsf/__init__.py
@@ -112,7 +112,8 @@ class Executor(RemoteExecutor):
             if exc.errno == 36:
                 wildcards_hash = hashlib.md5(wildcard_str.encode('utf8')).hexdigest()
                 lsf_logfile = os.path.abspath(
-                    f".snakemake/lsf_logs/{log_folder}/{wildcards_hash}/{self.run_uuid}.log"
+                    f".snakemake/lsf_logs/{log_folder}/"
+                    f"{wildcards_hash}/{self.run_uuid}.log"
                 )
                 os.makedirs(os.path.dirname(lsf_logfile), exist_ok=True)
             else:
